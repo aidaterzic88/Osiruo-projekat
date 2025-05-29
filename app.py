fd1eb9e5e5a00f2e198eeb398b78ee1b8a3cf176
aidaterzic88 <aida.terzic.24@size.ba>
2025-05-29 23:53:39 +0200

    pokusaj-50

--- app.py
+++ app.py
@@ -1,23 +1,18 @@
-from flask import Flask, render_template, redirect
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def root():
-    return redirect("/dobrodosli-u-projekat")
-
-@app.route("/dobrodosli-u-projekat")
 def home():
     return render_template("index.html")
 
 @app.route("/kontakt")
-def kontakt():
+def svemir():
     return render_template("kontakt.html")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
+
 
 
 
